85c41c072911abb43d7f1c2614278c699ac986d5
Jérôme Vieilledent <lolautruche@gmail.com>
2015-10-21 22:44:37 +0200

    Alarm activation: use variables for device names + updated inline doc

--- lua/script_device_securityAlarmActivation.lua
+++ lua/script_device_securityAlarmActivation.lua
@@ -2,17 +2,25 @@
 -- Activates Domoticz internal alarm and sends acknowledgement to the keypad.
 -- Arm status will depend on status of absence switch.
 
+keypadSwitch = 'Alarm Level' -- Mini keypad switch device name
+-- Scene to trigger to acknowledge alarm arming/disarming
+-- This scene should at least activate keypad "General" switch
+-- to send feedback from the Z-wave controller to the keypad.
+-- See Mini keypad documentation for more information.
+-- This scene can also e.g. switch on then off a light.
+acknoledgementScene = 'Alarm ack'
+
 commandArray = {}
-if (devicechanged['Alarm Level'] == 'On') then
+if (devicechanged[keypadSwitch] == 'On') then
 	if (otherdevices['Absence'] == 'On') then
         commandArray['Security panel'] = 'Arm Away'
     else
         commandArray['Security panel'] = 'Arm Home'
     end
-	commandArray['Scene:Alarm ack'] = 'On'
-elseif (devicechanged['Alarm Level'] == 'Off') then
+	commandArray['Scene:' .. acknoledgementScene] = 'On'
+elseif (devicechanged[keypadSwitch] == 'Off') then
 	commandArray['Security panel'] = 'Disarm'
-	commandArray['Scene:Alarm ack'] = 'On'
+	commandArray['Scene:' .. acknoledgementScene] = 'On'
 end
 
 return commandArray
